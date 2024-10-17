0084cc0ef32f2796c8cd0df60563d02fd5de2023
maornetzer9 <maorgithub@gmail.com>
2024-10-17 22:19:40 +0300

    Add configuration to vite to split modules to small chunks

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+    build: {
+      minify: false,  // Disable minification if not necessary
+      cssCodeSplit: true,  // Split CSS to prevent large chunks
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
+      },
+    },
+  }
+  
